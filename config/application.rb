681f4927abebb197cb946cfcbccfd61ee3875242
jkjellan <jkjellan@gmail.com>
2017-07-08 16:11:29 -0700

    Further CORS config

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,9 @@ module FindMyCarApi
     # config.api_only = true
     config.action_dispatch.default_headers = {
       'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+      'Access-Control-Request-Method' => '*',
+      'Access-Control-Allow-Headers' => '*',
+      'Access-Control-Allow-Methods' => '*'
     }
   end
 end
