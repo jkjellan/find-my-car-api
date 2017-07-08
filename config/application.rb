31fe37a9ece115199a03a67fa94418207f4de101
jkjellan <jkjellan@gmail.com>
2017-07-08 15:41:32 -0700

    Removes OPTIONS from Access-Control-Request-Method

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module FindMyCarApi
     config.api_only = true
     config.action_dispatch.default_headers = {
       'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => %w{GET POST PATCH DELETE OPTIONS}.join(",")
+      'Access-Control-Request-Method' => %w{GET POST PATCH DELETE}.join(",")
     }
   end
 end
