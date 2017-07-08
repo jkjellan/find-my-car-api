41a880b4d3adcf921b0b4ae6e65a9a4e08f4e361
jkjellan <jkjellan@gmail.com>
2017-07-08 16:06:26 -0700

    Allows CORS access from *

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module FindMyCarApi
     # Skip views, helpers and assets when generating a new resource.
     # config.api_only = true
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
+      'Access-Control-Allow-Origin' => '*',
       'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     }
   end
