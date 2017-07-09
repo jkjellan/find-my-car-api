0c60054c5a83010ed06113cdc103d092ed44dd91
jkjellan <jkjellan@gmail.com>
2017-07-09 12:59:53 -0700

    Attempts CORS configuration using rack-cors gem
    Previous method didn't work, so trying this. We'll see...

--- config/application.rb
+++ config/application.rb
@@ -29,11 +29,14 @@ module FindMyCarApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     # config.api_only = true
-    config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => '*',
-      'Access-Control-Allow-Headers' => '*',
-      'Access-Control-Allow-Methods' => '*'
-    }
+
+    # Rails 5
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :put, :delete]
+      end
+    end
+
   end
 end
